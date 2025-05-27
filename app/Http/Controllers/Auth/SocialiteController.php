f5889bf958f0d45f3a9e78dfc29147dc0e1ddeb7
roodey21 <peaceforabadon@gmail.com>
2025-05-27 10:33:43 +0700

    callback login with google

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
@@ -20,6 +22,16 @@ class SocialiteController extends Controller
 
         $user = User::where('email', $authenticatedUser->getEmail())->first();
 
-        dd($user);
+        if (!$user) {
+            $user = User::create([
+                'email' => $authenticatedUser->getEmail(),
+                'name' => $authenticatedUser->getName(),
+                'password' => Hash::make('password')
+            ]);
+        }
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
     }
 }
